4fa83a30a67a6b19b1e75756561b7d897a9e5159
Utkarsh Rasal <utkarsh.rasal@tekip.com>
2024-06-30 23:01:43 +0530

    removed secret keys

--- config/defaults.py
+++ config/defaults.py
@@ -1,6 +1,6 @@
 class Constants:
-    OPEN_AI_SECRET_KEY='sk-proj-kLEaBsJZ1IAezSyu1EyDT3BlbkFJHFtV4t2aV2lCKAM7vE0j'
-    PINECONE_SECRET_KEY='092c1fad-c7b6-4fd4-a3a0-f6287d775f55'
+    OPEN_AI_SECRET_KEY=''
+    PINECONE_SECRET_KEY=''
     PINECONE_INDEX = "engineer-embeddings"
 
 
@@ -8,9 +8,9 @@ class AppConfig:
     version: str = '1.0.0'
     app_name: str = "Querying Chatbot"
     debug: bool = True
-    database_url: str = "mongodb+srv://utkarshrasal:K3cYVC7kvw2ojjdv@mercor.rhkhw9v.mongodb.net/mercor?retryWrites=true&w=majority&appName=mercor"
+    database_url: str = ""
     # origins = [
     # "http://localhost:3000",
     # ]   
-    mongo_pass='K3cYVC7kvw2ojjdv'
+    mongo_pass=''
 config = AppConfig()
